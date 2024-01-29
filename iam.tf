0599f32683fe5d00a9adf23b657461dac082ea39
root <root@ip-172-31-42-27.ap-south-1.compute.internal>
2024-01-29 07:45:06 +0000

    adding terraform

---
+++ iam.tf
@@ -0,0 +1,55 @@
+data "aws_iam_policy" "ecs_task_execution_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "ecs_task_execution" {
+#  source_json = data.aws_iam_policy.ecs_task_execution_role_policy.policy
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:GetParameters",
+      "kms:Decrypt",
+      "secretsmanager:GetSecretValue",
+      "logs:CreateLogStream",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage",
+      "cloudwatch:*",
+      "xray:*"
+    ]
+    resources = ["*"]
+  }
+}
+
+module "ecs_task_execution_role" {
+  source     = "./modules/iam_role"
+  name       = "cstaskexecution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ecs_task_execution.json
+}
+
+data "aws_iam_policy_document" "ecs_task" {
+  statement {
+    actions = [
+      "ec2:Describe*",
+      "autoscaling:Describe*",
+      "ec2:CreateTags",
+      "appmesh:*",
+      "xray:*"
+    ]
+    resources = ["*"]
+  }
+}
+
+module "ecs_task_role" {
+  source     = "./modules/iam_role"
+  name       = "ecstask"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ecs_task.json
+}
